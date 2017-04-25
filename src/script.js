ddae3cf10c600cf3ff0c14223e5812cc83b9fc27
Windso <kristianwindso@gmail.com>
2017-04-25 02:41:26 -0700

    oops forgot one thing

--- src/script.js
+++ src/script.js
@@ -5,6 +5,9 @@ $("#send").click(function(){
 	    data: { "name": $("#name").val(), "email" : $("#email").val(), "message" : $("#message").val() },
 	    success: function() {
 	    	$("#submitted").show();
+	    	$("#name").val("");
+	    	$("#email").val("");
+	    	$("#message").val("");
 	    }
 	});
 });
