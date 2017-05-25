169d770922c3cee0617fe7b1cff817fbde84b775
Windso <kristianwindso@gmail.com>
2017-05-25 12:14:59 -0700

    added functionality to contact form requirements

--- src/script.js
+++ src/script.js
@@ -1,22 +1,40 @@
+
 $("#send").click(function(){
 	var name = $("#name").val();
 	var email = $("#email").val();
 	var message = $("#message").val();
 	if (isBlank(name) && isBlank(email) && isBlank(message)) {
-		// Nothing to send!
-		// Focus on Name
+		$(".contact-label").css("color", "#f00000");
+		$(".textbox").css("border-color", "#f00000");
+		$("#name").focus();
+		resetContactColors(7000);
 	} else if (isBlank(message)) {
-		// Please fill this out
-		// Focus on Message
+		$("#message-label").css("color", "#f00000");
+		$("#message").css("border-color", "#f00000");
+		$("#message").focus();
+		resetContactColors(7000);
 	} else if (isBlank(email)) {
-		// You didn't put an email address
-		// Let me fix it | Send anyways
+		$("#send").hide();
+		$("#no-email").fadeIn(200);
+		$(".yesno").fadeIn(200);
 	} else if(!isValidEmailAddress(email)) {
-	    // Your email doesn't look right
-	    // Let me fix it | Send anyways
+		$("#send").hide();
+		$("#bad-email").fadeIn(200);
+		$(".yesno").fadeIn(200);
 	} else {
 		sendMessage(name, email, message);
 	}
+});
+$("#send-yes").click(function(){
+	sendMessage($("#name").val(), $("#email").val(), $("#message").val());
+	hideYesNo();
+});
+$("#send-no").click(function(){
+	$("#email").focus();
+	hideYesNo();
+});
+$(".textbox").keypress(function() {
+	resetContactColors(0);
 });
 function isBlank (words) {
 	return words.replace(/\s+/g, '') == '';
@@ -25,6 +43,18 @@ function isValidEmailAddress (emailAddress) {
     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     return pattern.test(emailAddress);
 };
+function resetContactColors (wait) {
+	setTimeout( function() {
+		$(".contact-label").css("color", "#5a5959");
+		$(".textbox").css("border-color", "#bbb");
+	  }, wait);
+}
+function hideYesNo() {
+	$(".yesno").hide();
+	$("#no-email").hide();
+	$("#bad-email").hide();
+	$("#send").fadeIn(200);
+}
 function sendMessage (n,e,m) {
 	$.ajax({
 	    url: 'send-message.php',
