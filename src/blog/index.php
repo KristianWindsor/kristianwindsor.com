6463e90fdea44a78ed881c141a1179c94c698d5a
Kristian Windsor <kristianwindso@gmail.com>
2018-03-31 11:06:57 -0700

    created blog and first blog post

---
+++ src/blog/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>
+			Blog by Kristian Windsor
+		</title>
+		<link rel="stylesheet" href="blogfiles/blog.css">
+		<link rel="stylesheet" href="../style.css">
+		<link rel="apple-touch-icon" sizes="180x180" href="../icon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="../icon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="../icon/favicon-16x16.png">
+		<link rel="manifest" href="../icon/manifest.json">
+		<link rel="mask-icon" href="../icon/safari-pinned-tab.svg" color="#61cae9">
+		<link rel="shortcut icon" href="../icon/favicon.ico">
+		<meta name="msapplication-config" content="../icon/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+		<meta name="description" content="Blog by Kristian Windsor.">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	</head>
+	<body id="blog">
+		<div id="title">
+			<h1>
+				Blog by Kristian Windsor
+			</h1>
+		</div>
+		<div id="content">
+			<a href="why-you-should-start-a-blog" class="post-preview">
+				<img src="why-you-should-start-a-blog/laptop.jpg" />
+				<h3>
+					Why You Should Start a Blog
+				</h3>
+				<p>
+					Read about me trying super hard to justify the creation of this blog.
+				</p>
+			</a>
+		</div>
+	</body>
+	<script type="text/javascript" src="../script/jquery.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-92639312-2', 'auto');
+		ga('send', 'pageview');
+	</script>
+</html>
---
+++ src/blog/why-you-should-start-a-blog/index.php
@@ -0,0 +1,37 @@
+<?php
+	include '../blogfiles/header.html';
+?>
+<article id="article">
+	<header class="article-heading">
+		<h1 class="article-title">
+			Why You Should Start a Blog
+		</h1>
+		<h3 class="article-info">
+			By Kristian Windsor on March 18th, 2018
+		</h3>
+	</header>
+	<img src="laptop.jpg" class="image" />
+	<section>
+		<p>
+			This post is mostly targeted towards software developers, but still applies to everyone from dog walkers to graphic designers.
+		</p>
+		<p>
+			<span class="bold-text">You get a deeper understanding of the topics you write about.</span>
+			Writing about a topic ensures that you really understand that topic, no matter if you've worked with it for years or if its totally new to you. As you recite your own understanding, you begin to see it in a new light. Seeing in this new light, a fresh perspective, you may realize that you are missing a few pieces to the bigger picture. This is why people say the best way to learn something is to teach it.
+		</p>
+		<p>
+			<span class="bold-text">You're advertising yourself.</span>
+			While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer. You might find someone interested in helping you with that side project you've been working on. Any way it goes, having a blog can lead to many professional opportunities.
+		</p>
+		<p>
+			<span class="bold-text">You get to show off your skills and expertise.</span>
+			In order to give your name credibility, you need to show your work and talk about it. For example, instead of just listing PHP as one of your languages, you can talk about your experiences with PHP and the challenges you've overcame. You can talk about what you did and the reasoning behind each decision. 
+		</p>
+		<p>
+			All things considered, I would recommend starting your own blog.
+		</p>
+	</section>
+</article>
+<?php
+	include '../blogfiles/footer.html';
+?>
