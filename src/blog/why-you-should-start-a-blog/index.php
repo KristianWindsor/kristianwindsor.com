add50c6d60eefee65b6133676d617d080bdab3d9
Kristian Windsor <kristianwindso@gmail.com>
2018-04-04 21:40:02 -0700

    minor ui adjustments

--- src/blog/why-you-should-start-a-blog/index.php
+++ src/blog/why-you-should-start-a-blog/index.php
@@ -6,25 +6,25 @@
 		<h1 class="article-title">
 			Why You Should Start a Blog
 		</h1>
-		<h3 class="article-info">
+		<p class="article-info">
 			By Kristian Windsor on March 18th, 2018
-		</h3>
+		</p>
 	</header>
 	<img src="laptop.jpg" class="image" />
-	<section>
+	<section class="article-body">
 		<p>
 			This post is mostly targeted towards software developers, but still applies to everyone from dog walkers to graphic designers.
 		</p>
 		<p>
-			<span class="bold-text">You get a deeper understanding of the topics you write about.</span>
+			<span class="bold-text">You get a deeper understanding of the topics you write about.</span><br>
 			Writing about a topic ensures that you really understand that topic, no matter if you've worked with it for years or if its totally new to you. As you recite your own understanding, you begin to see it in a new light. Seeing in this new light, a fresh perspective, you may realize that you are missing a few pieces to the bigger picture. This is why people say the best way to learn something is to teach it.
 		</p>
 		<p>
-			<span class="bold-text">You're advertising yourself.</span>
-			While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer. You might find someone interested in helping you with that side project you've been working on. Any way it goes, having a blog can lead to many professional opportunities.
+			<span class="bold-text">You're advertising yourself.</span><br>
+			While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer, or a coding buddy. Any way it goes, having a blog can lead to many professional opportunities.
 		</p>
 		<p>
-			<span class="bold-text">You get to show off your skills and expertise.</span>
+			<span class="bold-text">You get to show off your skills and expertise.</span><br>
 			In order to give your name credibility, you need to show your work and talk about it. For example, instead of just listing PHP as one of your languages, you can talk about your experiences with PHP and the challenges you've overcame. You can talk about what you did and the reasoning behind each decision. 
 		</p>
 		<p>
