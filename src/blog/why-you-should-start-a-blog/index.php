128e2c48b7a864d43dae11bf992abeec0c9a2ce2
Kristian Windsor <kristianwindso@gmail.com>
2018-05-11 20:19:10 -0700

    cleaned up blog css

--- src/blog/why-you-should-start-a-blog/index.php
+++ src/blog/why-you-should-start-a-blog/index.php
@@ -17,24 +17,40 @@
 		<p>
 			This post is mostly targeted towards software developers, but still applies to everyone from dog walkers to graphic designers.
 		</p>
-		<p>
-			<span class="bold-text">You get a deeper understanding of the topics you write about.</span><br>
-			Writing about a topic ensures that you really understand that topic, no matter if you've worked with it for years or if its totally new to you. As you recite your own understanding, you begin to see it in a new light. Seeing in this new light, a fresh perspective, you may realize that you are missing a few pieces to the bigger picture. This is why people say the best way to learn something is to teach it.
-		</p>
-		<p>
-			<span class="bold-text">You're advertising yourself.</span><br>
-			While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer, or find a coding buddy. Any way it goes, having a blog can lead to many professional opportunities.
-		</p>
-		<p>
-			<span class="bold-text">You get to show off your skills and expertise.</span><br>
-			In order to give your name credibility, you need to show your work and talk about it. For example, instead of just listing PHP as one of your languages, you can talk about your experiences with PHP and the challenges you've overcame. You can talk about what you did and the reasoning behind each decision. 
-		</p>
-		<p>
-			If you're not a web developer or just don't want to design your own blog, there are tons of free blogging sites like <a href="https://www.blogger.com/">Blogger</a>.
-		</p>
-		<p>
-			If you do want to develop your own blog, you'll need a domain and a host. Personally I recommend <a href="https://www.namecheap.com/">Namecheap</a>. How you go about creating your blog is all up to you, depending on what languages you know and what languages you want to learn.
-		</p>
+		<div class="paragraph">
+			<p class="bold-text">
+				You get a deeper understanding of the topics you write about
+			</p>
+			<p>
+				Writing about a topic ensures that you really understand that topic, no matter if you've worked with it for years or if its totally new to you. As you recite your own understanding, you begin to see it in a new light. Seeing in this new light, a fresh perspective, you may realize that you are missing a few pieces to the bigger picture. This is why people say the best way to learn something is to teach it.
+			</p>
+		</div>
+			<div class="paragraph">
+			<p class="bold-text">
+				You're advertising yourself
+			</p>
+			<p>
+				While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer, or find a coding buddy. Any way it goes, having a blog can lead to many professional opportunities.
+			</p>
+			<div class="paragraph">
+			<p class="bold-text">
+				You get to show off your skills and expertise
+			</p>
+			<p>
+				In order to give your name credibility, you need to show your work and talk about it. For example, instead of just listing PHP as one of your languages, you can talk about your experiences with PHP and the challenges you've overcame. You can talk about what you did and the reasoning behind each decision. 
+			</p>
+		</div>
+			<div class="paragraph">
+				<p class="bold-text">
+					How to start your own blog
+				</p>
+			<p>
+				If you're not a web developer or just don't want to design your own blog, there are tons of free blogging sites like <a href="https://www.blogger.com/">Blogger</a>.
+			</p>
+			<p>
+				If you do want to develop your own blog, you'll need a domain and a host. Personally I recommend <a href="https://www.namecheap.com/">Namecheap</a>. How you go about creating your blog is all up to you, depending on what languages you know and what languages you want to learn.
+			</p>
+		</div>
 	</section>
 </article>
 <?php
