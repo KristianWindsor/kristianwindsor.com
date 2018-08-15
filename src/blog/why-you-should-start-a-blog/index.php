f1ad227fc09b7da9ac7028b231ae1a0f7209b49f
Kristian Windsor <kristianwindso@gmail.com>
2018-08-14 20:49:13 -0700

    revisted wyssab blog post

--- src/blog/why-you-should-start-a-blog/index.php
+++ src/blog/why-you-should-start-a-blog/index.php
@@ -19,7 +19,7 @@
 		</p>
 		<div class="paragraph">
 			<p class="bold-text">
-				You get a deeper understanding of the topics you write about
+				1. You get a deeper understanding of the topics you write about
 			</p>
 			<p>
 				Writing about a topic ensures that you really understand that topic, no matter if you've worked with it for years or if its totally new to you. As you recite your own understanding, you begin to see it in a new light. Seeing in this new light, a fresh perspective, you may realize that you are missing a few pieces to the bigger picture. This is why people say the best way to learn something is to teach it.
@@ -27,28 +27,28 @@
 		</div>
 			<div class="paragraph">
 			<p class="bold-text">
-				You're advertising yourself
+				2. You're advertising yourself
 			</p>
 			<p>
 				While you sleep, your blog is still up and easily accessible to anyone. You might catch the attention of a potential employer, or find a coding buddy. Any way it goes, having a blog can lead to many professional opportunities.
 			</p>
 			<div class="paragraph">
 			<p class="bold-text">
-				You get to show off your skills and expertise
+				3. You get to show off your skills and expertise
 			</p>
 			<p>
 				In order to give your name credibility, you need to show your work and talk about it. For example, instead of just listing PHP as one of your languages, you can talk about your experiences with PHP and the challenges you've overcame. You can talk about what you did and the reasoning behind each decision. 
 			</p>
 		</div>
-			<div class="paragraph">
-				<p class="bold-text">
-					How to start your own blog
-				</p>
-			<p>
-				If you're not a web developer or just don't want to design your own blog, there are tons of free blogging sites like <a href="https://www.blogger.com/">Blogger</a>.
+		<div class="paragraph">
+			<p class="bold-text">
+				Starting a blog
 			</p>
 			<p>
-				If you do want to develop your own blog, you'll need a domain and a host. Personally I recommend <a href="https://www.namecheap.com/">Namecheap</a>. How you go about creating your blog is all up to you, depending on what languages you know and what languages you want to learn.
+				If you're seeking to work in web development, this is a great opportunity to gain experience. You can use your choice of languages and tools to build your own blogging platform that also doubles as a portfolio piece.
+			</p>
+			<p>
+				An easier method of creating a blog is to use <a href="https://www.tumblr.com/" target="_blank">Tumblr</a>. Tumblr is a great platform for hosting blogs with text, images, and video. You have full control over the CSS of the page and it can easily be hosted on a custom domain name for $10/year. You can register a domain with <a href="https://www.namecheap.com/" target="_blank">Namecheap</a> and connect it to your tumblr blog using <a href="https://www.namecheap.com/support/knowledgebase/article.aspx/9247/2208/how-do-i-use-my-domain-with-my-tumblr-account" target="_blank">this guide</a>
 			</p>
 		</div>
 	</section>
