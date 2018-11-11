a2c0200d3b86cb2921ff44df249ccac8e928d07c
Kristian Windsor <kristianwindso@gmail.com>
2018-11-10 16:31:49 -0800

    changed email sender address to mail@

--- src/script/send-message.php
+++ src/script/send-message.php
@@ -8,8 +8,8 @@
 	$to      = "hello@kristianwindsor.com";
 	$subject = "KW: Message from " . $name;
     $message = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n" . $messageToSend;
-	$headers = 'From: hello@kristianwindsor.com' . "\r\n" .
-	    'Reply-To: hello@kristianwindsor.com' . "\r\n" .
+	$headers = 'From: mail@kristianwindsor.com' . "\r\n" .
+	    'Reply-To: mail@kristianwindsor.com' . "\r\n" .
 	    'X-Mailer: PHP/' . phpversion();
 
 	// send mail
