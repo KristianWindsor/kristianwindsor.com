cfff004917c82c9c27237e51c4a2446ba523f9fd
Kristian Windsor <kristianwindso@gmail.com>
2018-11-10 16:24:04 -0800

    use native php mail

--- src/script/send-message.php
+++ src/script/send-message.php
@@ -1,20 +1,17 @@
 <?php 
-	require 'mailgun/vendor/autoload.php';
-	use Mailgun\Mailgun;
-
+	// given data
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = "KW: Message from " . $name;
-    $message = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n" . $_POST['message'];
+    $messageToSend = $_POST['message'];
 
-	$mg = new Mailgun(getenv('MAILGUN_API_KEY'));
-	$domain = "kristianwindsor.com";
+    // define mail
+	$to      = "hello@kristianwindsor.com";
+	$subject = "KW: Message from " . $name;
+    $message = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n" . $messageToSend;
+	$headers = 'From: hello@kristianwindsor.com' . "\r\n" .
+	    'Reply-To: hello@kristianwindsor.com' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
 
-	$mg->sendMessage($domain, array(
-	'from'=>'mail@kristianwindsor.com',
-	'to'=> 'kristianwindso@gmail.com',
-	'subject' => $subject,
-	'text' => $message
-	    )
-	);
+	// send mail
+	mail($to, $subject, $message, $headers);
 ?>
