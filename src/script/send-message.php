2c4601f6c0e8eb036b18ae988101412921929e2c
Kristian Windsor <kristianwindso@gmail.com>
2019-07-09 00:07:35 -0700

    write messages to file on persistent volume

--- src/script/send-message.php
+++ src/script/send-message.php
@@ -4,41 +4,12 @@
     $email = $_POST['email'];
     $messageToSend = $_POST['message'];
 
-    // define mail
-	/*$to      = "hello@kristianwindsor.com";
-	$subject = "KW: Message from " . $name;
-    $message = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n" . $messageToSend;
-	$headers = 'From: mail@kristianwindsor.com' . "\r\n" .
-	    'Reply-To: mail@kristianwindsor.com' . "\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+    // write to file
+    $filename = '/var/www/html/messages/logs.txt';
+	file_put_contents($filename, '---'.PHP_EOL, FILE_APPEND | LOCK_EX);
+	file_put_contents($filename, $name.PHP_EOL, FILE_APPEND | LOCK_EX);
+	file_put_contents($filename, $email.PHP_EOL, FILE_APPEND | LOCK_EX);
+	file_put_contents($filename, $messageToSend.PHP_EOL, FILE_APPEND | LOCK_EX);
 
-	// send mail
-	mail($to, $subject, $message, $headers);*/
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
 
-	require './PHPMailer/src/Exception.php';
-	require './PHPMailer/src/PHPMailer.php';
-	require './PHPMailer/src/SMTP.php';
-
-	$mail = new PHPMailer(); // create a new object
-	$mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-	$mail->SMTPAuth = true; // authentication enabled
-	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-	$mail->Host = "smtp.gmail.com";
-	$mail->Port = 465; // or 587
-	$mail->IsHTML(false);
-	$mail->Username = "kristianwindsor2011@gmail.com";
-	$mail->Password = getenv('GMAIL_LOGIN');
-	$mail->SetFrom("kristianwindsor2011@gmail.com");
-	$mail->Subject = "Message from " . $name;
-	$mail->Body = "Name: " . $name . "\nEmail: " . $email . "\nMessage: " . $messageToSend;
-	$mail->AddAddress("hello@kristianwindsor.com");
-
-	if(!$mail->Send()) {
-		echo "Mailer Error: " . $mail->ErrorInfo;
-	} else {
-		echo "Message has been sent";
-	}
 ?>
