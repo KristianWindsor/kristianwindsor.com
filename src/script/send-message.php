bbfc394e68821bba929e51a2cc2bb123c8d23589
Kristian Windsor <kristianwindso@gmail.com>
2019-09-08 12:00:05 -0700

    integrated slack to deliver cnotact form submissions

--- src/script/send-message.php
+++ src/script/send-message.php
@@ -2,14 +2,25 @@
 	// given data
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $messageToSend = $_POST['message'];
+    $message = $_POST['message'];
+    $slackMessage = $_POST['name']."\n".$_POST['email']."\n".$_POST['message'];
 
-    // write to file
-    $filename = '/var/www/html/messages/logs.txt';
-	file_put_contents($filename, '---'.PHP_EOL, FILE_APPEND | LOCK_EX);
-	file_put_contents($filename, $name.PHP_EOL, FILE_APPEND | LOCK_EX);
-	file_put_contents($filename, $email.PHP_EOL, FILE_APPEND | LOCK_EX);
-	file_put_contents($filename, $messageToSend.PHP_EOL, FILE_APPEND | LOCK_EX);
+    $url = 'https://hooks.slack.com/services/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';
+    $data = array (
+        "token" => "XdiBqIMu8ELyzEIaxqzvbBsl",
+        "channel" => "#kristianwindsor",
+        "text"=> $slackMessage
+    );
+    $postdata = json_encode($data);
 
-
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    $result = curl_exec($ch);
+    curl_close($ch);
 ?>
