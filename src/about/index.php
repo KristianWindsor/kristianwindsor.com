1d459d4c99092a8dc553a1f0eec6fac69eb1e0a6
KristianWindsor <kristianwindso@gmail.com>
2020-12-24 13:12:30 -0800

    put nav in footer

--- src/about/index.php
+++ src/about/index.php
@@ -34,6 +34,23 @@
 						</div>
 					</div>
 				</div>
+				<div class="container">
+					<p>
+						<a href="https://github.com/KristianWindsor" target="_blank">
+							<i class="icon icon-github">&#xf300;</i> Github
+						</a>
+					</p>
+					<p>
+						<a href="https://www.linkedin.com/in/kristian-windsor/" target="_blank">
+							<i class="icon icon-linkedin">&#xf318;</i> LinkedIn
+						</a>
+					</p>
+					<p>
+						<a href="https://www.flickr.com/photos/kristianwindsor/" target="_blank">
+							<i class="icon icon-flickr">&#xf303;</i> Flickr
+						</a>
+					</p>
+				</div>
 			</div>
 		</div>
 		<?php
--- src/html/foot.php
+++ src/html/foot.php
@@ -2,21 +2,24 @@
 		<div class="foot">
 			<div class="links">
 				<div class="container">
-					<p>
-						<a href="https://github.com/KristianWindsor" target="_blank">
-							<i class="icon icon-github">&#xf300;</i> Github
-						</a>
-					</p>
-					<p>
-						<a href="https://www.linkedin.com/in/kristian-windsor/" target="_blank">
-							<i class="icon icon-linkedin">&#xf318;</i> LinkedIn
-						</a>
-					</p>
-					<p>
-						<a href="https://www.flickr.com/photos/kristianwindsor/" target="_blank">
-							<i class="icon icon-flickr">&#xf303;</i> Flickr
-						</a>
-					</p>
+					<a href="/">
+						home
+					</a>
+					<a href="/experience/">
+						experience
+					</a>
+					<a href="/portfolio/">
+						portfolio
+					</a>
+					<a href="/blog/">
+						blog
+					</a>
+					<a href="/about/">
+						about
+					</a>
+					<a href="/contact/">
+						contact
+					</a>
 				</div>
 			</div>
 		</div>
