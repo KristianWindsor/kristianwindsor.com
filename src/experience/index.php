cba4b6b543e7251a779db0625ebfc5263cbd6905
KristianWindsor <kristianwindso@gmail.com>
2020-12-28 20:57:35 -0800

    recognition & activities section

--- src/experience/index.php
+++ src/experience/index.php
@@ -107,12 +107,71 @@
 					</h2>
 				</div>
 				<div>
-					<ul>
-						<li>Presented a demo at DevOps World (2020)</li>
-						<li>Spoke on the DevOps World Podcast (2019)</li>
-						<li>Received the Rising Star Award on behalf of Pinger (2019)</li>
-						<li>Won the National STEM Challenge (2012)</li>
-					</ul>
+					<div class="project">
+						<div class="image-wrap">
+							<img src="/img/devops-world-2020.png" class="image partial nobig" title="Pinger Inc." alt="Pinger Inc." />
+						</div>
+						<div class="text-wrap">
+							<h3 class="title">
+								Presented a demo at DevOps World (2020)
+							</h3>
+							<p class="text">
+								"40 Second Prod"
+							</p>
+							<ul>
+								<li>
+									<a href="" target="_blank">
+										See the presentation on Cloudbees.com
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="project">
+						<div class="image-wrap">
+							<img src="/img/devops_radio.png" class="image partial nobig" title="Pinger Inc." alt="Pinger Inc." />
+						</div>
+						<div class="text-wrap">
+							<h3 class="title">
+								Featured on the DevOps Radio podcast (2019)
+							</h3>
+							<p class="text">
+								We (the DevOps team at Pinger) received the Rising Star Award on behalf of Pinger.
+							</p>
+							<ul>
+								<li>
+									<a href="https://open.spotify.com/episode/3gDSGVqLYJTKv6jL7uNAMd?si=mWFpZOR-S2S45ynrACRYAw" target="_blank">
+										Listen with Spotify
+									</a>
+								</li>
+								<li>
+									<a href="https://podcasts.apple.com/us/podcast/episode-61-pinger-a-devops-rising-star-is-born/id1108186102?i=1000453740190" target="_blank">
+										Listen with Apple Podcasts
+									</a>
+								</li>
+								<li>
+									<a href="https://devopsradio.libsyn.com/episode-61-a-devops-rising-star-is-born-adam-robertson-and-kristian-windsor-of-pinger" target="_blank">
+										Listen with LibSyn
+									</a>
+								</li>
+								<li>
+									<a href="https://www.businesswire.com/news/home/20190815005016/en/2019-DevOps-World-Jenkins-World-Award-Winners" target="_blank">
+										Business Wire
+									</a>
+								</li>
+								<li>
+									<a href="https://www.cloudbees.com/press/2019-devops-world-jenkins-world-award-winners-announced" target="_blank">
+										Cloudbees Press
+									</a>
+								</li>
+								<li>
+									<a href="" target="_blank">
+										Link
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="section experience">
