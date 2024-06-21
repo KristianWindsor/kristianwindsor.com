59abb57188c27e381282abd2a7dec7e35398172f
KristianWindsor <kristianwindso@gmail.com>
2024-06-21 00:22:12 -0700

    more pictures and a few edits

--- src/blog/custom-built-kitchen-for-my-lexus/index.php
+++ src/blog/custom-built-kitchen-for-my-lexus/index.php
@@ -21,7 +21,7 @@
 		</div>
 		<div class="paragraph">
 			<p>
-				Designed for road trips, car camping, and everyday use – I built a kitchen for my car – featuring a high-performance cooler, a pull-out water jug, and an extendable table
+				Designed for road trips, car camping, and everyday use – I built a kitchen for my car – featuring a high-performance cooler, a pull-out water jug, and an extendable table.
 			</p>
 		</div>
 		<div class="paragraph">
@@ -29,29 +29,24 @@
 				Pull-Out Water Jug
 			</h2>
 			<p>
-				One primary feature of my car's kitchen is the 3.5-gallon water jug with a faucet, ingeniously designed for convenience and practicality. Wrapped in high-quality insulation, this jug remains cool even during the hottest days, ensuring crisp and refreshing water at all times.
+				One primary feature of my car's kitchen is the 3.5-gallon water jug with a faucet. Wrapped in high-quality insulation, this jug remains cool even during the hottest days, ensuring crisp and refreshing water at all times.
 			</p>
+			<img src="water-jug.jpg" alt="Washing my pan after making oatmeal for breakfast" >
 			<p>
 				Mounted on smooth, sturdy rails, the water jug can be effortlessly pulled out of the car, preventing any splashes or spills inside the vehicle. This setup is incredibly versatile, making it perfect for refilling water bottles, boiling water for instant noodles, brushing your teeth, or even cleaning up pots and pans after a hearty meal.
 			</p>
-			<img src="water-jug.jpg" alt="Washing my pan after making oatmeal for breakfast" >
-			<p class="caption">
-				Washing my pan after making oatmeal for breakfast
-			</p>
+			<img src="water-jug-2.jpg" alt="Refilling my water bottle" >
 		</div>
 		<div class="paragraph">
 			<h2>
 				High-Performance Cooler
 			</h2>
 			<p>
-				Another key feature of my car's kitchen is the high-performance cooler, meticulously designed to preserve the freshness and quality of your food, even for perishables like milk and raw meat. Encased in up to 5 inches of R-5 insulation foam, this outperforms Yeti coolers and anything else on the market.
-			</p>
-			<p>
-				The cooler is equipped with a Bluetooth-enabled thermometer, allowing you to monitor the internal temperature directly from your phone. In addition to providing peace of mind with the knowledge that your food is at a safe temperature, this also helps keep the temperature down by preventing the cooler from being opened unnecessarily.
+				Another key feature of my mini kitchen is the high-performance cooler, meticulously designed to preserve the freshness of your food, even for perishables like milk and raw meat. Encased in up to 5 inches of R-5 insulation foam, this outperforms Yeti coolers and anything else on the market.
 			</p>
 			<img src="cooler.jpg" alt="Opening the cooler to grab some dinner" >
-			<p class="caption">
-				Opening up the cooler to grab some dinner
+			<p>
+				The cooler is equipped with a Bluetooth-enabled thermometer, allowing you to monitor the internal temperature directly from your phone. In addition to providing peace of mind with the knowledge that your food is at a safe temperature, this also helps keep the temperature down by preventing the cooler from being opened unnecessarily.
 			</p>
 		</div>
 		<div class="paragraph">
@@ -59,19 +54,20 @@
 				Versatile Slide-Out Table
 			</h2>
 			<p>
-				Another prominent feature is that this whole thing doubles as a table, providing a convenient surface to cook on, prepare meals, or enjoy your food. 
+				Another notable feature is that it doubles as a table, offering a convenient surface for setting up your portable stove or enjoying your meal.
 			</p>
 			<img src="table.jpg" alt="Boiling water for instant ramen" >
-			<p class="caption">
-				Boiling some water for instant ramen in Death Valley
+			<p>
+				Local taco trucks often lack seating areas, making this a convenient spot for eating without messing up your car's interior. Similarly, when grabbing pizza from Costco's food court, this place offers a quiet and pleasant alternative to Costco's loud and crowded seating area.
 			</p>
+			<img src="costco.jpg" alt="Costco pizza" >
 		</div>
 		<div class="paragraph">
 			<h2>
 				Modular Design
 			</h2>
 			<p>
-				The cooler and the water jug can both be easily removed for restocking and for cleaning. These pieces along with several others are held in place through interlocking friction, gravity, and magnets – much like the way LEGO pieces fit together.
+				The cooler and the water jug can both be easily removed for restocking and for cleaning. Every piece is built like LEGO – held in place through interlocking friction, gravity, and magnets.
 			</p>
 			<img class="isgif" src="modular.jpg" >
 		</div>
@@ -80,7 +76,7 @@
 				Seamless Lexus Styling
 			</h2>
 			<p>
-				Beyond its functionality, every detail of this mini kitchen is styled to seamlessly integrate with the aesthetic of this car. The exterior is styled with matching OEM colors and materials, featuring ivory leather wrapping on the face and sides, a wood grain vinyl wrap on the table top, and an OEM cargo mat that's been cut out and mounted to the lid. Even the interior components have been diligently spray-painted black, ensuring a cohesive and premium look.
+				Beyond its functionality, every detail of this build is styled to seamlessly integrate with the aesthetic of this car. The exterior is styled with matching OEM colors and materials, featuring ivory leather wrapping on the face and sides, a wood grain vinyl wrap on the table top, and an OEM cargo mat that's been cut out and mounted to the lid. Even the interior components have been spray-painted black, ensuring a cohesive and premium look.
 			</p>
 			<img src="style.jpg" alt="Lexus Kitchen styling" >
 		</div>
@@ -89,7 +85,7 @@
 				The Journey of Creation: Learning by Doing
 			</h2>
 			<p>
-				When I embarked on the project of building my mini kitchen, I was stepping into uncharted territory. With no prior experience in woodworking, insulation, or vinyl wrapping, I had to rely on a slow process of trial and error. Each step presented a new challenge, but with patience and persistence, I navigated through them.
+				When I embarked on the project of building a kitchen for my car, I was stepping into uncharted territory. With no prior experience in woodworking, insulation, or vinyl wrapping, I had to rely on a slow process of trial and error. Each step presented a new challenge, but with patience and persistence, I navigated through them.
 			</p>
 			<img src="wip1.jpg" alt="Lexus Kitchen, WIP" >
 			<p>
@@ -105,10 +101,10 @@
 			<p>
 				There are many other details that took an absurb amount of time, such as using frictionless tape to make the moving pieces buttery-smooth, mounting steel braces to reinforce the foam walls, and installing three layers of seal strips around the opening of the cooler.
 			</p>
+			<img src="wip3.jpg" alt="Lexus Kitchen, WIP" >
 			<p>
 				But eventually I forced myself to look past the imperfections and move on to finish the project.
 			</p>
-			<img src="wip3.jpg" alt="Lexus Kitchen, WIP" >
 		</div>
 		<div class="paragraph">
 			<h2>
@@ -117,6 +113,7 @@
 			<p>
 				Creating this project was an incredibly rewarding journey, filled with both challenges and triumphs. What started as a simple idea inspired by a YouTube video evolved into a fully functional setup that perfectly complements my car adventures. Each step, from designing and selecting materials to overcoming the trials of woodworking and insulation, taught me valuable skills and deepened my appreciation for hands-on craftsmanship.
 			</p>
+			<img src="pulledover.jpg" >
 			<p>
 				Whether you're a seasoned DIY enthusiast or a beginner like I was, I hope my journey inspires you to take on your own projects and discover the joy of creating something from scratch.
 			</p>
