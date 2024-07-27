3561f1244e6e2e9d5342c179ba64c8c4f97bcc94
KristianWindsor <kristianwindso@gmail.com>
2024-07-27 01:20:45 -0700

    final edits

--- src/blog/custom-built-kitchen-for-my-lexus/index.php
+++ src/blog/custom-built-kitchen-for-my-lexus/index.php
@@ -29,11 +29,11 @@
 				Pull-Out Water Jug
 			</h2>
 			<p>
-				One primary feature of my car's kitchen is the 3.5-gallon water jug with a faucet. Wrapped in high-quality insulation, this jug remains cool even during the hottest days, ensuring crisp and refreshing water at all times.
+				Mounted on smooth, sturdy rails, a 3.5-gallon water jug with a built-in faucet effortlessly slides out of the car, where it can be used for refilling water bottles, boiling water for dinner, brushing your teeth, or even for cleaning up pots and pans after a hearty meal.
 			</p>
 			<img src="water-jug.jpg" alt="Washing my pan after making oatmeal for breakfast" >
 			<p>
-				Mounted on smooth, sturdy rails, the water jug can be effortlessly pulled out of the car, preventing any splashes or spills inside the vehicle. This setup is incredibly versatile, making it perfect for refilling water bottles, boiling water for instant noodles, brushing your teeth, or even cleaning up pots and pans after a hearty meal.
+				Wrapped in high-quality insulation, this jug remains cool even during the hottest days, ensuring crisp and refreshing water at all times.
 			</p>
 			<img src="water-jug-2.jpg" alt="Refilling my water bottle" >
 		</div>
@@ -42,11 +42,11 @@
 				High-Performance Cooler
 			</h2>
 			<p>
-				Another key feature of my mini kitchen is the high-performance cooler, meticulously designed to preserve the freshness of your food, even for perishables like milk and raw meat. Encased in up to 5 inches of foam board insulation, this modified Coleman cooler is comparable in performance to name-brand coolers like Yeti.
+				Encased in up to 5 inches of foam board insulation, this modified Coleman cooler is comparable in performance to name-brand coolers like Yeti. It will preserve the freshness of your food for days – even for perishables like milk and raw meat.
 			</p>
 			<img src="cooler.jpg" alt="Opening the cooler to grab some dinner" >
 			<p>
-				The cooler is equipped with a Bluetooth-enabled thermometer, allowing you to monitor the internal temperature directly from your phone. In addition to providing peace of mind with the knowledge that your food is at a safe temperature, this also helps keep the temperature down by preventing the cooler from being opened unnecessarily.
+				The cooler is equipped with a Bluetooth-enabled thermometer, allowing you to monitor the internal temperature directly from your phone, providing peace of mind with the knowledge that your food is at a safe temperature.
 			</p>
 			<img src="bluetooth.jpg" alt="Using my phone to view the cooler's temperature" >
 		</div>
@@ -55,35 +55,35 @@
 				Versatile Slide-Out Table
 			</h2>
 			<p>
-				Another notable feature is that it doubles as a table, offering a convenient surface for setting up your portable stove or enjoying your meal.
+				This also doubles as a table, offering a convenient surface for setting up your portable stove or enjoying a meal on the road.
 			</p>
 			<img src="table.jpg" alt="Boiling water for instant ramen" >
 			<p>
-				Local taco trucks often lack seating areas, making this a convenient spot for eating without messing up your car's interior. Similarly, when grabbing pizza from Costco's food court, this place offers a quiet and pleasant alternative to Costco's loud and crowded seating area.
+				This impromptu table also comes in handy when local taco trucks don't provide seating areas, or when Costco's food court is too loud and crowded.
 			</p>
 			<img src="costco.jpg" alt="Costco pizza" >
-		</div>
-		<div class="paragraph">
-			<h2>
-				Modular Design
-			</h2>
-			<p>
-				The cooler and the water jug can both be easily removed for restocking and for cleaning. Every piece is built like LEGO – held in place through interlocking friction, gravity, and magnets.
-			</p>
-			<img class="isgif" src="modular-design.jpg" >
 		</div>
 		<div class="paragraph">
 			<h2>
 				Seamless Lexus Styling
 			</h2>
 			<p>
-				Beyond its functionality, every detail of this build is styled to seamlessly integrate with the aesthetic of this car. The exterior is styled with matching OEM colors and materials, featuring ivory leather wrapping on the face and sides and an OEM cargo mat that's been cut out and mounted to the lid. Even the interior components have been spray-painted black, ensuring a cohesive look.
+				Beyond its functionality, every detail of this build is styled to match the car's colors and materials, featuring ivory leather wrapping on the sides and an OEM cargo mat on the top. Even the interior components have been spray-painted black to ensure a cohesive look.
 			</p>
 			<img src="style.jpg" alt="Lexus Kitchen styling" >
 			<p>
-				The tabletop is hand-carved from plywood and wrapped in wood grain vinyl to match the car's wood trim interior, giving it a premium look.
+				The tabletop is hand-carved from plywood and wrapped in wood grain vinyl, matching the car's wood trim interior and giving it a premium look.
 			</p>
 			<img src="table-style.jpg" alt="A beautiful wood grain vinyl wrap" >
+		</div>
+		<div class="paragraph">
+			<h2>
+				Modular Design
+			</h2>
+			<p>
+				The cooler and water jug can both be removed for easy cleaning and restocking. Every piece is built like LEGO – held in place through interlocking friction, gravity, and magnets.
+			</p>
+			<img class="isgif" src="modular-design.jpg" >
 		</div>
 		<div class="paragraph">
 			<h2>
@@ -93,10 +93,10 @@
 				I am proud of this project, but it is by no means perfect.
 			</p>
 			<p>
-				I wanted it to be portable so that it'd be easy to remove from my car when the space is needed for something else. Unfortunately, due to the choice in materials (lots of wood), the final product is way too heavy to be easily moved. If I were to build this again, I would try to use more lightweight materials like plastic or 3D printer filament instead of solid wood.
+				I aimed for portability so it could be easily removed from my car when needed. However, my choice of materials, primarily wood, made the final product too heavy to move effortlessly. If I were to rebuild it, I would opt for lighter materials like plastic or 3D printer filament instead of solid wood.
 			</p>
 			<p>
-				Another failed goal was for this cooler to outperform name-brand coolers like Yeti. Although I was successful in greatly improving performance of the $20 Coleman cooler that I used, it was not enough to beat a Yeti. If I were to build this again, I would simply start with a high-performance cooler instead of trying to create my own.
+				Another failed goal was for this cooler to outperform name-brand coolers like Yeti. Although I was successful in greatly improving performance of the <a href="https://a.co/d/c3yXOAW" target="_blank">$20 Coleman cooler</a> that I started with, it was not enough to beat a Yeti. If I were to rebuild this project, I'd simply purchase a high-performance cooler instead of going the DIY route and trying to create my own.
 			</p>
 		</div>
 		<div class="paragraph">
@@ -104,21 +104,35 @@
 				The Journey of Creation: Learning by Doing
 			</h2>
 			<p>
-				When I embarked on the project of building a kitchen for my car, I was stepping into uncharted territory. With no prior experience in woodworking, insulation, or vinyl wrapping, I had to rely on a slow process of trial and error. Each step presented a new challenge, but with patience and persistence, I navigated through them.
+				When I first started on this project, I was stepping into uncharted territory. With no prior experience in woodworking, insulation, or anything of the sort, I had to rely on a slow process of trial and error.
 			</p>
 			<img src="wip1.jpg" alt="Lexus Kitchen, WIP" >
 			<p>
-				At first it seemed relatively straightforward – just mount the rails with screws and mount the foam insulation with glue – easy peasy.
+				At first it seemed relatively straightforward. "Just mount the rails, mount the insulation, and call it a day," I thought.
 			</p>
 			<img src="wip2.jpg" alt="Lexus Kitchen, WIP" >
 			<p>
-				However, I quickly realized that I had committed myself to resolving countless issues, diving into an endless pursuit of perfection and getting lost in the details.
+				However, I quickly realized the complexity involved and found myself committed to resolving countless issues in an endless pursuit of perfection.
 			</p>
 			<p>
-				For example, to optimize the cooler's efficiency I ended up creating exact molds of the cooler and water jug using spray insulation foam. First I slathered the cooler with cooking oil to prevent the foam from sticking (a very messy process), then set the cooler into place with a 40 lb dumbbell on top for stability, then sprayed foam everywhere to seal the gaps, and finally waited 6 hours for the foam to harden. I repeated this process over a dozen times in order to achieve the perfect mold.
+				For example, to optimize the cooler’s performance, I created an exact mold of the cooler using spray insulation foam in a very crude process involving a tub of canola oil, a 40 lb dumbbell, a bread knife, and a lot of patience.
 			</p>
 			<p>
-				There are many other details that took an absurb amount of time, such as using frictionless tape to make the moving pieces buttery-smooth, mounting steel braces to reinforce the foam walls, and installing three layers of seal strips around the opening of the cooler.
+				There are many other details hidden underneath the kitchen's faux leather skin:
+				<ul>
+					<li>
+						<a href="https://a.co/d/bgM2qhp" target="_blank">UHMW polyethylene tape</a> to allow the entire project to slide out smoothly and quietly
+					</li>
+					<li>
+						<a href="https://a.co/d/4RqTeQ5" target="_blank">Neodymium magnets</a> to catch sliding pieces and coax them into place
+					</li>
+					<li>
+						<a href="https://a.co/d/fkZdBF6" target="_blank">Steel braces</a> to reinforce the foam walls
+					</li>
+					<li>
+						<a href="https://a.co/d/40sze5R" target="_blank">2mm rubber seal strips</a> to close any air gaps around the cooler
+					</li>
+				</ul>
 			</p>
 			<img src="wip3.jpg" alt="Lexus Kitchen, WIP" >
 			<p>
@@ -130,11 +144,20 @@
 				Conclusion
 			</h2>
 			<p>
-				Creating this project was an incredibly rewarding journey, filled with both challenges and triumphs. What started as a simple idea inspired by a YouTube video evolved into a fully functional setup that perfectly complements my car adventures. Each step, from designing and selecting materials to overcoming the trials of woodworking and insulation, taught me valuable skills and deepened my appreciation for hands-on craftsmanship.
+				The purpose of this project was never actually about having a kitchen for my car. I'm proud of what I built, but honestly it's not very practical considering how big and heavy it is. The same functionality could be achieved with just a few products from Amazon. No – the true purpose of this project was simply just to create something.
+			</p>
+			<p>
+				There's something magical about the process of creating something – from the moment an idea first pops into my mind to the moment I show the finished product to a friend with a proud, "I made this." The journey of bringing an idea to life is an adventure that never fails to captivate me.
+			</p>
+			<p>
+				Building this also left me with a valuable new skillset that I can apply to future projects or simply put to use in repairing things around my apartment. For me, learning through hands-on experience like this is more effective and more enjoyable than formal classes or lengthy YouTube tutorials.
+			</p>
+			<p>
+				Lastly, I love that this kitchen acts an excuse to take a friend on an adventure – be it a road trip to Death Valley, camping in Lake Tahoe, or just a trip to Costco.
 			</p>
 			<img src="camping.jpg" alt="Camping" >
 			<p>
-				Whether you're a seasoned DIY enthusiast or a beginner like I was, I hope my journey inspires you to take on your own projects and discover the joy of creating something from scratch.
+				I hope that my journey inspires you to take on your own projects and discover the joy of creating something from scratch.
 			</p>
 			<p>
 				Thanks for reading!
