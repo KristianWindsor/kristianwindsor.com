df8a1cf8561c69edaf2aac3e4a3aebb0ae336ed9
KristianWindsor <kristianwindso@gmail.com>
2024-12-31 19:04:20 -0800

    email blog post rough draft

---
+++ src/blog/the-story-behind-my-unique-email-address/index.php
@@ -0,0 +1,151 @@
+<?php
+	$articleTitle = "The Story Behind My Unique Email Address";
+	$articleDescription = "Way shorter than your email";
+	$articleUrl = "the-story-behind-my-unique-email-address";
+	$imageUrl = "buzz-lightyear-disneyland.jpg";
+	$datePublished = "2025-01-01";
+	include '../blogfiles/header.php';
+?>
+<article id="article">
+	<header class="article-heading">
+		<h1 class="article-title">
+			<?php echo $articleTitle ?>
+		</h1>
+		<p class="article-info">
+			By Kristian Windsor on January 1st, 2025
+		</p>
+	</header>
+	<section class="article-body">
+		<div class="paragraph">
+			<img src="<?php echo $imageUrl ?>" class="image" alt="Entering email at Buzz Lightyear ride at Disneyland" />
+			<p>
+				My email address –  k@wnd.sr – is super short and fully functional.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				Losing the Perfect Email Address
+			</h2>
+			<p>
+				When I was nine years old, I created my first email address: KristianWindsor@gmail.com. It was elegantly simple – just my first and last name paired with a modern "@gmail.com" domain. Unfortunately, I quickly lost the password and, despite my best efforts, was never able to recover the account.
+			</p>
+			<p>
+				What followed was a string of attempts to find a new, equally satisfying email address. Each attempt felt imperfect in its own way:
+			</p>
+			<ul>
+				<li>I tried adding numbers,	kristianwindsor2011@gmail.com, but it felt random and unprofessional.</li>
+				<li>I tried shortening it by removing the trailing "r" for kristianwindso@gmail.com, but people always thought it was a typo.</li>
+				<li>I tried using my own domain, like hello@kristianwindsor.com, but it felt cold and impersonal – more like a customer support email than a personal one.</li>
+			</ul>
+			<p>
+				This endless quest for the perfect email address became a personal challenge: I wanted something unique, concise, and reflective of my identity.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				Fascination with Unique TLDs
+			</h2>
+			<p>
+				My fascination with unique and unconventional top-level domains (TLDs) fueled my search. While most people are familiar with traditional TLDs like .com, .net, and .org – or newer ones like .io, .ai, and .xyz – there are over 1,500 TLDs in existence today, each offering exciting possibilities.
+			</p>
+			<p>
+				Some major brands  like Google, Nike, and Lamborghini even use their own custom TLDs to reflect exclusivity, strengthen their brand identity, and showcase innovation. For instance:
+			</p>
+			<ul>
+				<li><a href="https://about.google/" target="_blank">about.google</a></li>
+				<li><a href="https://www.swoosh.nike/" target="_blank">swoosh.nike</a></li>
+				<li><a href="https://esports.lamborghini/" target="_blank">esports.lamborghini</a></li>
+			</ul>
+			<p>
+				The plethora of modern TLDs allow brands to integrate their name directly with the domain, creating a sleek, memorable identity. Here are some of my favorite brand/domain name combinations:
+			</p>
+			<ul>
+				<li><a href="https://cash.app/" target="_blank">cash.app</a></li>
+				<li><a href="https://sexy.pizza/" target="_blank">sexy.pizza</a></li>
+				<li><a href="https://bright.green/" target="_blank">bright.green</a></li>
+			</ul>
+			<p>
+				You can also get creative with <a href="https://en.wikipedia.org/wiki/Domain_hack" target="_blank">domain hacks</a>, where the FQDN forms readable words or phrases:
+			</p>
+			<ul>
+				<li><a href="https://cr.yp.to/" target="_blank">cr.yp.to</a></li>
+				<li><a href="https://en.wikipedia.org/wiki/Delicious_(website)" target="_blank">del.icio.us</a></li>
+				<li><a href="https://e.xplo.it/" target="_blank">e.xplo.it</a></li>
+			</ul>
+			<p>
+				This inspired me to explore the idea of having my very own distinctive domain.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				Discovering the wnd.sr Domain
+			</h2>
+			<p>
+				I liked the idea of domain hacking and started searching for a TLD I could use for my name. During my search, I stumbled upon the <a href="https://en.wikipedia.org/wiki/.sr" target="_blank">.sr</a> TLD, which is the country code for <a href="https://en.wikipedia.org/wiki/Suriname" target="_blank">Suriname</a>, a Dutch-speaking nation in South America.
+			</p>
+			<p>
+				To my astonishment, the domain wnd.sr was available! It's short and sweet – exactly what I was looking for.
+			</p>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7817996.430002706!2d-57.33029739715854!3d6.323498049394742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8d0836d5d9d3d551%3A0x1037e1c0712936b2!2sSuriname!5e0!3m2!1sen!2sus!4v1735597775776!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			<p>
+				Although, the process to acquire the wnd.sr domain wasn’t exactly straightforward. I was confused at first because I wasn't able to buy the .sr domain on NameCheap, GoDaddy, or any common domain registrars. Eventually I came to learn that these domains can only be purchased through <a href="https://datasur.sr/domains/" target="_blank">Datasur</a>, a subsidiary of the Surinamese state-owned telecom company, Telesur.  I had to pay for it using an international bank transfer, which came with a hefty $40 fee imposed by Chase. Thankfully, the staff at Datasur was incredibly accommodating and allowed me to pay for five years upfront in a single transaction to minimize fees. In the end, the domain costs me about $25 per year – a small price for something so unique.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				Choosing an Email Service
+			</h2>
+			<p>
+				Initially, I considered experimenting with free or self-hosted email solutions. However, I was concerned about the reliability and opted for using Gmail which costs $6/month.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				It's Short!
+			</h2>
+			<p>
+				My previous email address, kristian@kristianwindsor.com, spanned a lengthy 28 characters. By contrast, my new email, k@wnd.sr, is a mere 8 characters long.
+			</p>
+			<p>
+				Typing it out is quick and effortless, which comes in handy when autofill isn’t available, like on self-serve restaurant tablets or DMV forms. It’s a small but meaningful convenience in everyday life.
+			</p>
+			<img src="shake-shack.jpg" alt="Entering my email at Shake Shack to get my receipt" >
+		</div>
+		<div class="paragraph">
+			<h2>
+				Personal Branding
+			</h2>
+			<p>
+				Beyond practicality, my email address serves as a subtle yet powerful element of personal branding. Its uniqueness makes it a memorable addition to my resume, hopefully catching the attention of potential employers. If I had business cards, it would undoubtedly become a conversation starter.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				The Risks
+			</h2>
+			<p>
+				Unfortunately, <a href="https://en.wikipedia.org/wiki/Domain_Name_System_Security_Extensions" target="_blank">DNSSEC</a>, a security protocol that can help protect against DNS spoofing, isn’t available for .sr domains. This limitation does pose potential risks, such as increased vulnerability to phishing attacks or unauthorized interception of emails.
+			</p>
+			<p>
+				Additionally, some email validators on the Internet could falsely flag this as an invalid email address because of how short it is or because of the unusual TLD.
+			</p>
+			<p>
+				Despite these potential challenges, I remain confident in my choice, appreciating the distinctiveness and functionality of my email.
+			</p>
+		</div>
+		<div class="paragraph">
+			<h2>
+				Conclusion
+			</h2>
+			<p>
+				My email address, k@wnd.sr, represents more than just a way to contact me – it’s a testament to my creativity, persistence, and love for unique solutions. And, honestly, I just think it's really cool.
+			</p>
+			<p>
+				Thanks for reading!
+			</p>
+		</div>
+	</section>
+</article>
+<?php
+	include '../blogfiles/footer.php';
+?>
