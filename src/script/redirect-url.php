a837c63aad764e054c97ae9f7e196295b5894b58
KristianWindsor <kristianwindso@gmail.com>
2025-02-01 15:03:24 -0800

    fixed redirect

--- src/script/redirect-url.php
+++ src/script/redirect-url.php
@@ -1,13 +1,7 @@
 <?php
 	if($_SERVER['SERVER_NAME'] == 'wnd.sr') {
-		if($_SERVER['REQUEST_URI'] == '/lexus-kitchen') {
-			$destination = 'https://kristianwindsor.com/blog/custom-built-kitchen-for-my-lexus/';
-		} else {
-			$destination = 'https://kristianwindsor.com/';
-		}
-		
 		header("HTTP/1.1 301 Moved Permanently");
-		header("Location: " . $destination);
+		header("Location: https://kristianwindsor.com/");
 		exit();
 	}
 ?>
