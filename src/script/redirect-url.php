5a97a096a0253581ab441bf743c0ab7b604b6822
KristianWindsor <kristianwindso@gmail.com>
2025-02-01 14:48:38 -0800

    add lexus-kitchen url shortcut

--- src/script/redirect-url.php
+++ src/script/redirect-url.php
@@ -1,5 +1,13 @@
 <?php
 	if($_SERVER['SERVER_NAME'] == 'wnd.sr') {
-		header('Location: https://kristianwindsor.com/');
+		if($_SERVER['REQUEST_URI'] == '/lexus-kitchen') {
+			$destination = 'https://kristianwindsor.com/blog/custom-built-kitchen-for-my-lexus/';
+		} else {
+			$destination = 'https://kristianwindsor.com/';
+		}
+		
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: " . $destination);
+		exit();
 	}
 ?>
