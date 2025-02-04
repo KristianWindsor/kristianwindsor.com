987e14eed512c14d68cee6dea827a9136d702edd
KristianWindsor <kristianwindso@gmail.com>
2025-02-03 17:11:52 -0800

    center footer

--- src/html/foot.php
+++ src/html/foot.php
@@ -1,31 +1,29 @@
 
 		<div class="foot">
 			<div class="links">
-				<div class="container">
-					<a href="/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe804;</i></p>
-						<p><span>HOME</span></p>
-					</a>
-					<a href="/experience/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe809;</i></p>
-						<p><span>EXPERIENCE</span></p>
-					</a>
-					<a href="/portfolio/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe806;</i></p>
-						<p><span>PORTFOLIO</span></p>
-					</a>
-					<a href="/blog/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe807;</i></p>
-						<p><span>BLOG</span></p>
-					</a>
-					<a href="/contact/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe803;</i></p>
-						<p><span>CONTACT</span></p>
-					</a>
-					<a href="/about/" class="icon-link">
-						<p><i class="icon icon-flickr">&#xe808;</i></p>
-						<p><span>ABOUT</span></p>
-					</a>
-				</div>
+				<a href="/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe804;</i></p>
+					<p><span>HOME</span></p>
+				</a>
+				<a href="/experience/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe809;</i></p>
+					<p><span>EXPERIENCE</span></p>
+				</a>
+				<a href="/portfolio/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe806;</i></p>
+					<p><span>PORTFOLIO</span></p>
+				</a>
+				<a href="/blog/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe807;</i></p>
+					<p><span>BLOG</span></p>
+				</a>
+				<a href="/contact/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe803;</i></p>
+					<p><span>CONTACT</span></p>
+				</a>
+				<a href="/about/" class="icon-link">
+					<p><i class="icon icon-flickr">&#xe808;</i></p>
+					<p><span>ABOUT</span></p>
+				</a>
 			</div>
 		</div>
